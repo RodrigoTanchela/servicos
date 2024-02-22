2d2d05297011068c8bb9bacd1e6f2fe047a261c4
RodrigoTanchela <rmtanchela@gmail.com>
2024-02-21 22:46:48 -0300

    servicos

---
+++ book-service/src/main/java/br/com/rodrigo/controller/BookController.java
@@ -0,0 +1,73 @@
+package br.com.rodrigo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.rodrigo.model.Book;
+import br.com.rodrigo.proxy.CambioProxy;
+import br.com.rodrigo.repository.BookRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Book endpoint")
+@RestController
+@RequestMapping("book-service")
+public class BookController {
+	
+	@Autowired
+	private Environment environment;
+	
+	@Autowired
+	private BookRepository repository;
+	
+	@Autowired
+	private CambioProxy proxy;
+	
+	@Operation(summary = "Find a specific book by your ID")
+	@GetMapping(value = "/{id}/{currency}")	
+	public Book findBook(
+			@PathVariable("id") Long id,
+			@PathVariable("currency") String currency
+			) {
+		var book = repository.getById(id);
+		if (book == null) throw new RuntimeException("Book not Found");
+		
+		var cambio = proxy.getCambio(book.getPrice(), "USD", currency);
+		
+		var port = environment.getProperty("local.server.port");
+		book.setEnvironment("Book port: " + port + " Cambio Port " + cambio.getEnvironment());
+		book.setPrice(cambio.getConvertedValue());
+		book.setEnvironment(port+ " FEIGN");
+		return book;
+	}
+	
+//	@GetMapping(value = "/{id}/{currency}")	
+//	public Book findBook(
+//			@PathVariable("id") Long id,
+//			@PathVariable("currency") String currency
+//			) {
+//		var book = repository.getById(id);
+//		if (book == null) throw new RuntimeException("Book not Found");
+//		
+//		HashMap<String, String> params = new HashMap<>();
+//		params.put("amount", book.getPrice().toString());
+//		params.put("from", "USD");
+//		params.put("to", currency);
+//		
+//		//Anotar
+//		var response = new RestTemplate()
+//			.getForEntity("http://localhost:8000/cambio-service/"+ "{amount}/{from}/{to}", Cambio.class, params);
+//		
+//		var cambio = response.getBody();
+//		
+//		var port = environment.getProperty("local.server.port");
+//		book.setPrice(cambio.getConvertedValue());
+//		book.setEnvironment(port);
+//		return book;
+//	}
+
+}
